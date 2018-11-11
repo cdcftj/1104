2acae5700ae996550e9d43181941b907751225a0
Tan Jin <tanjin2004@126.com>
2018-11-12 00:05:42 +0800

    change

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from mysite.views import hello,current_datetime
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)), #正则表达式，各个配置的名称都是大写的，TEMPLATE_DIRS
+    #固定内容，定位符。点号.是除了\n都可以匹配，*前面一个字符匹配零次或多次 ?零次或一次 +一次或多次，
     url(r'^hello/$', hello),
     url(r'^time/$',current_datetime),
 ]
